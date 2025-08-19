17595e1ea69f1cbe792e1cfb7f05583e4b5bf843
planit-evy <EvheniyY@planit-inc.com>
2025-08-19 11:33:47 +0000

    Deploying to gh-pages from @ planit-evy/viewer-npm-test@a3b7bc14c050d0192d65a7c3bc141112bdaf6ff1 🚀

--- assets/react-18-DpUxKJX8.js
+++ assets/react-18-DpUxKJX8.js
@@ -1,4 +1,4 @@
-import{a as k1,r as Fc}from"./iframe-GjqgdF5z.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
+import{a as k1,r as Fc}from"./iframe-CUgFqwJq.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
  * @license React
  * scheduler.production.js
  *
